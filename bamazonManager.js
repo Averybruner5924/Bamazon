7509ac3b7397c3735f5bb657ef50722e3ff0ad80
Averybruner5924 <averybruner@yahoo.com>
2018-04-25 16:38:18 -0500

    Finished Management

--- bamazonManager.js
+++ bamazonManager.js
@@ -33,6 +33,113 @@ function start() {
 
     ])
     .then(function(answer) {
-        console.log(answer.menu)
-    })
+       var managerRequest = answer.menu;
+       if (managerRequest === "View Products for Sale") {
+        connection.query("SELECT * FROM products", function(err, res) {
+          if (err) throw err;
+          console.log(res);
+          connection.end();
+        })
+      };
+      if (managerRequest === "View Low Inventory") {
+        connection.query("SELECT * FROM products WHERE quantity < 10", function(err, res) {
+          if (err) throw err;
+          console.log(res);
+          connection.end();
+        });
+      };
+      if (managerRequest === "Add to Inventory") {
+        addInventory();
+       
+      };
+      if (managerRequest === "Add New Product") {
+        addProduct();
+      };
+    });
   }
+
+    
+  function addInventory() {  
+    inquirer
+    .prompt([
+      {
+    name: "id",
+    type: "input",
+    message: "What is the id of the product?"
+    },
+    {
+    name: "quantity",
+    type: "input",
+    message: "What is the new quantity?"
+    },
+   ])
+   
+  .then(function(answer) {
+      var customerQuantity = answer.quantity;
+      var customerId = answer.id;
+
+        var query = connection.query(
+          "UPDATE products SET ? WHERE ?",
+          [
+            {
+              quantity: customerQuantity
+            },
+            {
+              item_id: customerId
+            }
+          ],
+    function(err, res) {
+      connection.end();
+    }
+  );
+});
+}
+
+function addProduct() {  
+  inquirer
+  .prompt([
+    {
+  name: "name",
+  type: "input",
+  message: "What is the new product?"
+  },
+  {
+  name: "department",
+  type: "input",
+  message: "What is the department?"
+  },
+  {
+  name: "price",
+  type: "input",
+  message: "What is the price?"
+  },
+  {
+  name: "quantity",
+  type: "input",
+  message: "What is the quantity?"
+  },
+ ])
+ 
+.then(function(answer) {
+    var productName = answer.name;
+    var productDepartment = answer.department;
+    var productPrice = answer.price;
+    var productQuantity = answer.quantity;
+
+
+      var query = connection.query(
+        "INSERT INTO products SET ?",
+        [
+          {
+            product_name: productName,
+            department_name: productDepartment,
+            price: productPrice,
+            quantity: productQuantity
+          }
+        ],
+  function(err, res) {
+    connection.end();
+  }
+);
+});
+}
